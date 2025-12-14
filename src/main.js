a242ac8f7502417269f5ec81ee765a906faa1e82
adrita50 <adrita.das2020@gmail.com>
2025-12-14 20:42:06 +0600

    home page

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import './tailwind.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+ 
+//type indicator in text
+
+const textElement = document.getElementById('typing-text');
+const text = 'Technology, Startup, LifeStyle';
+
+
+
+let index = 0;
+const speed = 50;
+const pauseTime = 2000;
+
+  
+
+function typeWriter() {
+  if(index < text.length){
+
+    textElement.textContent +=text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
+
+  else{
+    setTimeout(()=>{
+      textElement.textContent = ''
+      index=0;
+      typeWriter();
+    },
+  pauseTime);
+  }
+}
+
+typeWriter();
+
+ 
