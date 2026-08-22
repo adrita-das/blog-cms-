21cece300d7eea170eec2cfbd688229ee5382fd1
adrita50 <adrita.das2020@gmail.com>
2026-08-23 00:12:21 +0600

    Add multi-page entry points to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
 
 export default defineConfig({
   plugins: [
@@ -8,6 +10,20 @@ export default defineConfig({
    server: {
     port: 5173,
     open: true
+  },
+   
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        auth: resolve(__dirname, 'auth.html'),
+        post: resolve(__dirname, 'post.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        read: resolve(__dirname, 'read.html'),
+        stories: resolve(__dirname, 'stories.html'),
+      }
+    }
   }
-})
+  })
 
